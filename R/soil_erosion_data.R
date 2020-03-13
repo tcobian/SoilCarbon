87792856ae1a6228db61e99be28039177bc9bbee
Tyler Cobian <tylercobian@Tylers-MacBook-Air-4.local>
2020-03-12 17:51:56 -0700

    Finished?

---
+++ R/soil_erosion_data.R
@@ -0,0 +1,10 @@
+#' Example data set of soil erosion rates
+#'
+#' \describe(
+#' \item(year){time elapsed in years}
+#' \item(land_area){Hecatares of land accounted for}
+#' \item(erosion.year){kg of soil that have been lost since year 1})
+#'
+#'
+
+"soil_erosion_data"
